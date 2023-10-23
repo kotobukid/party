ad1d61af5d9599303555b1adacabfe71e02ebf64
kotobukid <kotobuki@daqua.jp>
2023-10-23 23:22:47 +0900

    見た目調整

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -25,7 +25,7 @@ onMounted(async () => {
         <col style="width: 150px;"/>
         <col style="width: 300px;"/>
         <col style="width: 200px;"/>
-        <col style="width: 600px;"/>
+        <col style="width: 345px;"/>
     </colgroup>
     <thead>
     <tr>
@@ -39,7 +39,7 @@ onMounted(async () => {
     <tr v-for="e in greetMsg">
         <td>{{ e.time }}</td>
         <td><a target="_blank" :href="e.shop_link">{{ e.shop_name }}</a></td>
-        <td>{{ e.format }}</td>
+        <td :data-format="e.format">{{ e.format }}</td>
         <td>{{ e.name }}</td>
     </tr>
     </tbody>
@@ -48,7 +48,7 @@ onMounted(async () => {
 
 <style scoped>
 table {
-    width: 1460px;
+    width: 1170px;
     table-layout: fixed;
     border-collapse: collapse;
 }
@@ -59,10 +59,30 @@ th, td {
 }
 
 th {
+    color: white;
     background-color: #121212;
 }
 
 td {
     text-align: left;
+}
+
+tr:hover {
+    background-color: lightgreen;
+}
+
+a {
+    color: darkblue;
+    font-weight: bolder;
+}
+
+td[data-format="オールスター"] {
+    background-color: pink;
+}
+td[data-format="キーセレクション"] {
+    background-color: orange;
+}
+td[data-format="ディーヴァセレクション"] {
+    background-color: lightblue;
 }
 </style>
